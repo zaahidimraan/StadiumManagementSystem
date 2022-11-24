ca4d17b8d4733a96d34a295739224ee2cb6a71d9
Zahid <i200469@nu.edu.pk>
2022-11-24 22:52:37 +0500

    Controller of FXML added

---
+++ src/application/Match.java
@@ -0,0 +1,55 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class Match {
+
+	 @FXML
+	    private TableColumn<?, ?> colStaffCNIC;
+
+	    @FXML
+	    private TableColumn<?, ?> colStaffDuty;
+
+	    @FXML
+	    private TableColumn<?, ?> colStaffName;
+
+	    @FXML
+	    private TableColumn<?, ?> colStaffSalary;
+
+	    @FXML
+	    private TableColumn<?, ?> colStaffType;
+
+	    @FXML
+	    private Button staffAdd;
+
+	    @FXML
+	    private Button staffBack;
+
+	    @FXML
+	    private TextField staffCNIC;
+
+	    @FXML
+	    private Button staffDelete;
+
+	    @FXML
+	    private TextField staffDuty;
+
+	    @FXML
+	    private TextField staffName;
+
+	    @FXML
+	    private TextField staffSalary;
+
+	    @FXML
+	    private Button staffSalaryM;
+
+	    @FXML
+	    private TextField staffType;
+
+	    @FXML
+	    private Button staffUpdate;
+
+}
